00bab677988703fb05c7003528263861cc8c7f37
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2023-03-20 10:19:32 +0530

    social media card section  added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,41 @@ import React from "react";
 import CircleIcon from "./CircleIcon";
 import "./Contact.scss";
 import Marquee from "react-fast-marquee";
+import { AiOutlineInstagram, AiOutlineBehance } from "react-icons/ai";
+import { CiTwitter } from "react-icons/ci";
+import { FiFacebook } from "react-icons/fi";
+import { SecondaryBtn } from "./Btn";
+
+const socialData = [
+  {
+    icon: function () {
+      return <AiOutlineInstagram />;
+    },
+    text: "instagram",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+  },
+  {
+    icon: function () {
+      return <CiTwitter />;
+    },
+    text: "Twitter",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+  },
+  {
+    icon: function () {
+      return <FiFacebook />;
+    },
+    text: "Facebook",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+  },
+  {
+    icon: function () {
+      return <AiOutlineBehance />;
+    },
+    text: "Behance",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+  },
+];
 
 const Contact = () => {
   return (
@@ -29,7 +64,21 @@ const Contact = () => {
           <span>Follow us on social media</span>
         </Marquee>
       </div>
-      
+
+      <div className="container-3">
+        {socialData.map((elem, index) => (
+          <div className="item" key={index}>
+            <div className="upper">
+              {elem.icon()}
+              <SecondaryBtn classText={"btn-s-45"} text={""} />
+            </div>
+            <div className="lower">
+              <h3>{elem.text}</h3>
+              <p>{elem.desc.split(0, 180)}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
