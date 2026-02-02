a843a8b61dd7299ea923d713b51cb419562e27e0
SM8UTI <smrutiranjannayak5403@gmail.com>
2026-02-02 19:32:38 +0530

    Add cobrowse-sdk-js for co-browsing support and update package dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import CobrowseIO from "cobrowse-sdk-js";
 import App from "./App";
 import "./index.scss";
+
+// Cobrowse.io – co-browsing support
+CobrowseIO.license = "3DktgwtXuOCMSA";
+CobrowseIO.start();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
